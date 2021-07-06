ebe99a6c1861da0d2b920de3cd381dd2899788d7
Benja <otienojaoko@gmail.com>
2021-07-06 19:29:10 +0300

    update

--- variables.py
+++ variables.py
@@ -1,3 +1,7 @@
+# a variable is a container that can store values
+# Each variable has a name, a type and a value stored in it
+# Names are like the label on a cardboard box
+# Type include integers, floats, doubles, strings and char
 print ("hello world")
  a = 1234
  b = 99
