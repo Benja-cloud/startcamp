f5124bfae892569442d579630f6920c640135286
Benja <otienojaoko@gmail.com>
2021-07-07 21:16:26 +0300

    update

--- conditions.py
+++ conditions.py
@@ -1,5 +1,10 @@
-a = 33
+a = 330
 b = 200
 if b > a:
 	print("b is greater than a")
-
+else:
+	print("a is greater than b")
+#if test whether the condition is true(executes before run time)
+# while executes during run time
+# if there are only two conditions use if ...... else
+#else tells the programmes that its comes to the end
