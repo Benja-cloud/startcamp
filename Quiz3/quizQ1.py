623e9747e8acdd09e6cfc9b0f59ef7ff5fac5c88
Benja <otienojaoko@gmail.com>
2021-07-08 17:07:13 +0300

    update

---
+++ Quiz3/quizQ1.py
@@ -0,0 +1,12 @@
+#what is the difference between '=' and '=='
+#Answer: '=' is used to assign a value while '==' is for comparison
+# the code below when run shows this
+x = 3
+y = 5
+
+if x == y:
+    print('they are equal')
+else:
+    print('they are not equal')
+
+#the result is : they are not equal. This shows the use of each
---
+++ Quiz3/quizQ2.py
@@ -0,0 +1,9 @@
+#can I leave out the collon in an if,while or for statement? YES/NO
+# Answer: NO
+# leaving the colon leads to a syntax error
+
+for i in range(8)
+    print(i)
+
+#running the code above validates that
+
