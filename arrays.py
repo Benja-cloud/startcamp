2fb7d16b3cca6bd03a7399c8a9afc3817471a819
Benja <otienojaoko@gmail.com>
2021-07-12 17:37:14 +0300

    update

--- arrays.py
+++ arrays.py
@@ -2,5 +2,14 @@ cars = ["BMW","bugatti","Volvo","Merc"]
 cars.remove("Volvo")
 cars.pop(2)
 cars.append("toyota")
-cars.clear()
+#cars.clear()
+print(cars)
+x=cars.count("toyota")
+print(cars)
+
+y = len(cars)
+print(y)
+
+cars.sort(reverse=True)
+cars.sort()
 print(cars)
