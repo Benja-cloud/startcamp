b9936cdec1216e02470cdeba2d4eba47fb17a5e5
Benja <otienojaoko@gmail.com>
2021-07-16 20:12:08 +0300

    commit

--- Quiz1/quizQ1.py
+++
@@ -1,4 +0,0 @@
-#which of the following is the correct way to execute userargument.py
-#the following are correct ways
-python3 userargument.py
-python userargument.py
--- Quiz1/quizQ5.py
+++
@@ -1,6 +0,0 @@
-a = b = c = 17
-print(a)
-print(b)
-print(c)
-
-# results show that the equation is true
